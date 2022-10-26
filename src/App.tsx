358e840a4a91f93883674dbf9d562d7a2a21e230
Martin Ferreira <martin.f.yic@gmail.com>
2022-10-25 22:20:28 -0300

    [add] HangImage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
 import "./App.css";
+import { HangImage } from "./components/HangImage";
+import { letters } from "./helpers/letters";
 
 function App() {
+    const [word] = useState("COMPUTADORA");
+    const [hidenWord] = useState("_ ".repeat(word.length));
+    const [attempts, setAttempts] = useState(0);
+
+    const checkLetter = (letterSelected: string) => {
+        console.log(letterSelected);
+        setAttempts(Math.min(attempts + 1, 9));
+    };
+
     return (
         <div className="App">
-            <h1>Hola mundo</h1>
+            <h1>Ahorcado</h1>
+
+            <HangImage imageNumber={attempts} />
+
+            <h2>{hidenWord}</h2>
+
+            <h3>Intentos: {attempts}</h3>
+
+            <div>
+                {letters.map((letter) => (
+                    <button onClick={() => checkLetter(letter)} key={letter}>
+                        {letter}
+                    </button>
+                ))}
+            </div>
         </div>
     );
 }
